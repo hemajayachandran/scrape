d4b87c9b25a94d0f78bba60de052105760d5f153
hemajayachandran <hema.jayachandran.a@gmail.com>
2020-06-25 00:40:20 +0530

    added code to identify the category of the product

--- test.py
+++ test.py
@@ -78,6 +78,38 @@ for link in link_list:
         related_products.append(pdt.get_text())
     print("Related Products: ",related_products)
 
+    #----------------Product Features----------------
+    navbar = soup_level2.find('div', {'class': 'navbar__container'})
+    pdt_dropdown = navbar.find_all('li')
+    category_types = {}
+    for drp_div in pdt_dropdown[0].find_all('div', {'class': 'category-types__item'}):
+        category_types[(drp_div.get_text(strip=True).replace('\n', ''))] = []
+
+    #---------------Product Dropdowns;Category, Gender/Life Stage, Health Benefits-----------------
+    drp_item_div = pdt_dropdown[0].find('div', {'class': 'categories'})
+    item_div = drp_item_div.find_all('div', {'class': 'categories__col'})
+    for item in item_div[0].find_all('a'):
+        #print(item_div[0])
+        category_types['Category'].append(item.get_text(strip=True).replace('\n', ''))
+    for item in item_div[1].find_all('a'):
+        #print(item_div[0])
+        category_types['Gender / Life\xa0Stage'].append(item.get_text(strip=True).replace('\n', ''))
+    for item in item_div[2].find_all('a'):
+        #print(item_div[0])
+        category_types['Health Benefits'].append(item.get_text(strip=True).replace('\n', ''))
+
+    #---------------Other Categories--------------------
+    other_categories_div = soup_level2.find('div', {'class': 'product__categories'})
+    product_features = {'Category': [], 'Gender / Life\xa0Stage': [], 'Health Benefits': []}
+    for link in other_categories_div.find_all('a'):
+        if link.get_text().replace('\n', '') in category_types['Category']:
+            product_features['Category'].append(link.get_text().replace('\n', ''))
+        elif link.get_text().replace('\n', '') in category_types['Gender / Life\xa0Stage']:
+            product_features['Gender / Life\xa0Stage'].append(link.get_text().replace('\n', ''))
+        else:
+            product_features['Health Benefits'].append(link.get_text().replace('\n', ''))
+    print("Categories: ",product_features)
+
     #driver.execute_script("window.history.go(-1)")
     back_button = driver.find_element_by_xpath("//a[@href='https://newrootsherbal.ca/en/products']")
     driver.execute_script("arguments[0].click();", back_button)
